a3c587be055fa04e420ee3283f1fcce68987d14d
Noah Sabbagh <61517057+s462947@users.noreply.github.com>
2020-04-22 11:01:45 -0400

    AddedSVG

--- SampleVideoGameNoahS/main_menu.lua
+++ SampleVideoGameNoahS/main_menu.lua
@@ -49,20 +49,20 @@ local instructionsButton
 
 -- Creating Transition Function to Credits Page
 local function CreditsTransition( )       
-    composer.gotoScene( "credits_screen", {effect = "flipFadeOutIn", time = 500})
+    composer.gotoScene( "credits_screen", {effect = "fade", time = 1000})
 end 
 
 -----------------------------------------------------------------------------------------
 
 -- Creating Transition to Level1 Screen
 local function Level1ScreenTransition( )
-    composer.gotoScene( "level1_screen", {effect = "zoomInOutFade", time = 1000})
+    composer.gotoScene( "level1_screen", {effect = "slideUp", time = 1000})
 end    
 
 -----------------------------------------------------------------------------------------
 
 local function InstructionsTransition( )
-    composer.gotoScene( "instructions_screen", {effect = "zoomInOutFade", time = 1000})
+    composer.gotoScene( "instructions_screen", {effect = "slideLeft", time = 1000})
 end
 
 -----------------------------------------------------------------------------------------
